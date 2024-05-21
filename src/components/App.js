03ff89f7795e6dd24483414b19082f9b39502915
Chandler Abril <abrilchandler1@gmail.com>
2024-05-20 20:47:16 -0700

    Additional organizing

--- src/components/App.js
+++ src/components/App.js
@@ -24,22 +24,28 @@ function App() {
   return (
     <div>
     <NavBar />
+
     <Switch>
     <Route path="/golden">
       <Golden data={data}/>
     </Route>
+
     <Route path="/massmonster">
       <MassMonster data={data}/>
     </Route>
+
     <Route path="/modern">
       <Modern data={data}/>
     </Route>
+
     <Route path="/bronze">
       <Bronze data={data}/>
     </Route>
+
     <Route path="/">
       <Home data={data} setData={setData}/>
     </Route>
+    
     </Switch>
     </div>
   )
